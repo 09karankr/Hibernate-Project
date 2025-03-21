a37acdbda8d6c4939d4570419fd21d45986236ef
Karan Kumar <officialkaran39@gmail.com>
2025-03-21 16:07:57 +0530

    [Karan]Add. added the configuration and successfully save the data to the postgresql with hibernate

--- src/main/java/com/hibproject/Main.java
+++ src/main/java/com/hibproject/Main.java
@@ -1,7 +1,28 @@
 package com.hibproject;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
 
+        Student s1 = new Student();
+        s1.setsName("Karan");
+        s1.setRollNo(101);
+        s1.setsAge(22);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(com.hibproject.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.save(s1);
+        transaction.commit();
+        System.out.println(s1);
     }
 }
